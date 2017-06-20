e825482623fdec4099e7a6018b42f71d30939193
DiYi <646747453@qq.com>
2017-06-20 15:24:04 +0800

    3

---
+++ app/src/main/java/com/example/zhang/haha/Utils.java
@@ -0,0 +1,15 @@
+package com.example.zhang.haha;
+
+/**
+ * Created by Zhang on 2017/6/20.
+ */
+
+public class Utils {
+    String haha;
+    public Utils(String haha){
+        this.haha = haha;
+    }
+    public void get(){
+
+    }
+}
